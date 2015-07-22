c3b2552cc0ed35a97c1c72ab58f4722097705a0f
umizawa <ayoanuzemu.2240@gmail.com>
2015-07-23 00:06:27 +0900

    First commit

---
+++ app/src/main/java/com/example/naoya/todomanager/CellData.java
@@ -0,0 +1,14 @@
+package com.example.naoya.todomanager;
+
+import java.util.Date;
+
+/**
+ * Created by Naoya on 2015/07/22.
+ */
+public class CellData {
+
+    private int imageResourceId;
+    private int importance;
+    private Date editDay, dueDay;
+    private String title, message;
+}
---
+++ app/src/main/java/com/example/naoya/todomanager/ToDoData.java
@@ -0,0 +1,14 @@
+package com.example.naoya.todomanager;
+
+import java.util.Date;
+
+/**
+ * Created by Naoya on 2015/07/23.
+ */
+public class ToDoData {
+    private int imageResourceId;
+    private int importance;
+    private boolean repeat;
+    private String title, place, comment;
+    private Date edit,due;
+}
