e1ffc1ce802cc9afc7730c42cc092533ad456fd8
umizawa <ayoanuzemu.2240@gmail.com>
2015-07-27 18:21:05 +0900

    Added icons.

--- app/src/main/java/com/example/naoya/todomanager/MainActivity.java
+++ app/src/main/java/com/example/naoya/todomanager/MainActivity.java
@@ -8,7 +8,12 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
-    @Override
+    public static final int MENU_ITEM0 = 0;
+    public static final int MENU_ITEM1 = 1;
+    public static final int MENU_ITEM2 = 2;
+    public static final int MENU_ITEM3 = 3;
+
+    @Override                                                                                       //アクティビティ起動時に呼び出し
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -17,7 +22,24 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+        super.onCreateOptionsMenu(menu);
+
+        MenuItem item0 = menu.add(0,MENU_ITEM0,0,"アイテム0");
+        item0.setIcon(android.R.drawable.ic_menu_camera);
+
+        MenuItem item1 = menu.add(0,MENU_ITEM1,0,"アイテム1");
+        item1.setIcon(android.R.drawable.ic_menu_call);
+
+        MenuItem item2 = menu.add(0,MENU_ITEM2,0,"アイテム2");
+        item2.setIcon(android.R.drawable.ic_menu_add);
+        item2.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+
+        MenuItem item3 = menu.add(0,MENU_ITEM3,0,"アイテム3");
+        item3.setIcon(android.R.drawable.ic_menu_delete);
+        item3.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
