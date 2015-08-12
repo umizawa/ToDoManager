df10a804179fa69359d396b15318e3b7a6730786
umizawa <ayoanuzemu.2240@gmail.com>
2015-08-12 23:35:47 +0900

    added realm db getters and setters.

--- app/src/main/java/com/example/naoya/todomanager/ToDoData.java
+++ app/src/main/java/com/example/naoya/todomanager/ToDoData.java
@@ -2,13 +2,40 @@ package com.example.naoya.todomanager;
 
 import java.util.Date;
 
-/**
- * Created by Naoya on 2015/07/23.
- */
-public class ToDoData {
+import io.realm.RealmObject;
+
+public class ToDoData extends RealmObject{
+    private String title;
+    private String place;
+    private String comment;
     private int imageResourceId;
     private int importance;
-    private boolean repeat;
-    private String title, place, comment;
-    private Date edit,due;
+    private boolean repeatFlag;
+    private Date editDay;
+    private Date dueDay;
+    private byte repeatDay[] = new byte[7];
+
+    public String getTitle(){return title;}
+    public void setTitle(String title){this.title = title;}
+    public String getPlace(){return place;}
+    public void setPlace(String place){this.place = place;}
+    public String getComment(){return comment;}
+    public void setComment(String comment){this.comment = comment;}
+    public int getImageResourceId(){return imageResourceId;}
+    public void setImageResourceId(int imageResourceId){this.imageResourceId = imageResourceId;}
+    public int getImportance(){return importance;}
+    public void setImportance(int importance){this.importance = importance;}
+    public boolean getRepeatFlag(){return repeatFlag;}
+    public void setRepeatFlag(boolean repeatFlag){this.repeatFlag = repeatFlag;}
+    public Date getEditDay(){return editDay;}
+    public void setEditDay(Date editDay){this.editDay = editDay;}
+    public Date getDueDay(){return dueDay;}
+    public void setDueDay(Date dueDay){this.dueDay = dueDay;}
+    public byte getRepeatDay(int day){
+        if (0 <= day && day <= 6)return repeatDay[day];
+        else return -1;}
+    public void setRepeatDay(int day, byte repeatDay){
+        if (0 <= day && day <= 6)this.repeatDay[day] = repeatDay;
+    }
 }
+
